e468af64b6fcb2e5af38dc64349e12098e6da312
kaielaw <kaylahessaferdinan@gmail.com>
2025-11-10 23:32:27 +0700

    initial work

---
+++ 251101_4_Toki_1_4.cpp
@@ -0,0 +1,10 @@
+/*
+Date : 1 November 2025
+Desc : SPak Dengklek memiliki 738 ekor bebek jantan dan 519 ekor bebek betina. Berapakah total banyaknya bebek Pak Dengklek?
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Pak Dengklek memiliki " << 738 + 519 << " ekor bebek." << std::endl;
+}
